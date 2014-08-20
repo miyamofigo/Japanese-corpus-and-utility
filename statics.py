a89168d82a967b822c084fa1c6df8ef99283ce7e
Takeo Miyamoto <miyamofigo@gmail.com>
2014-08-20 18:27:04 +0900

    added pearson correlation coefficient function.

--- statics.py
+++ statics.py
@@ -15,7 +15,7 @@ from nltk.corpus.util import *
 from nltk.text import Text
 from nltk.probability import *
 
-from util import *
+#from util import *
 from numpy import array
 from scipy.stats import chi2_contingency
 
@@ -41,3 +41,25 @@ def test_independency(feature, condition, cfdist,
     return True
   else:
     return False
+
+from math import sqrt
+def pearson(v1, v2):
+	"""
+This function is quoted from 'Programming Collective Intelligence', Oreilly Books,
+Toby Segaran(Author), 978-4-87311-364-7.
+In strictly speaking, this is a correlation coefficient with inner product.
+	"""
+	sum1 = sum(v1)
+	sum2 = sum(v2)
+
+	sum1Sq = sum([pow(v,2) for v in v1])
+	sum2Sq = sum([pow(v,2) for v in v2])
+
+	pSum = sum([v1[i]*v2[i] for i in range(len(v1))])
+
+	num = pSum - (sum1 * sum2 / len(v1))
+	den = sqrt((sum1Sq - pow(sum1, 2) / len(v1)) * (sum2Sq - pow(sum2, 2) / len(v2)))
+	if den == 0:
+		return 0
+	return 1.0 - num/den
+
