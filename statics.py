72094f450b2e873e7a310267ccb79e16e06d601b
Takeo Miyamoto <miyamofigo@gmail.com>
2014-08-26 21:31:41 +0900

    added kcluster function.

--- statics.py
+++ statics.py
@@ -63,3 +63,41 @@ In strictly speaking, this is a correlation coefficient with inner product.
 		return 0
 	return 1.0 - num/den
 
+def kcluster(rows, distance=pearson, k=4): 
+	"""
+This function is quoted from 'Programming Collective Intelligence', Oreilly Books,
+Toby Segaran(Author), 978-4-87311-364-7.
+It is a kind of K-means clustering function.
+	"""
+	ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+						for i in range(len(rows[0])]
+	clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+							 for i in range(len(rows[0])) for j in range(k)] for j in range(k)]
+	latmatches = None
+
+	for t in range(100):
+		print 'Iteration %d' % t 
+		bestmatche = [[] for in range(k)]
+
+		for j in range(len(rows)):
+			row = rows[j]
+			bestmatch = 0
+			for i in range(k):
+				d = distance(clusters[i], row)
+				if d < distance(clusters[bestmatch], row):
+					bestmatch = i
+				bestmatches[bestmatch].append(j)
+			if bestmatches == latmatches:
+				break
+			latmatches = bestmatches
+
+		for i in range(k):
+			avgs = [0.0] * len(rows[0])
+			if len(bestmatches[i]) > 0:
+				for rowid in bestmatches[i]:
+					for m in range(len(rows[rowid])):
+						avgs[m] += rows[rowid][m]
+					for j inh range(len(avgs)):
+						avgs[j] /= len(bestmatches[i])
+					clusters[i] = avgs
+		return bestmatches
