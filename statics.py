88a574053cbc7afcbc35c42952644ea8ba69a8a4
Takeo Miyamoto <miyamofigo@gmail.com>
2014-08-28 01:09:39 +0900

    fixed some bugs with kcluster function.

--- statics.py
+++ statics.py
@@ -63,6 +63,7 @@ In strictly speaking, this is a correlation coefficient with inner product.
 		return 0
 	return 1.0 - num/den
 
+import random
 def kcluster(rows, distance=pearson, k=4): 
 	"""
 This function is quoted from 'Programming Collective Intelligence', Oreilly Books,
@@ -70,14 +71,14 @@ Toby Segaran(Author), 978-4-87311-364-7.
 It is a kind of K-means clustering function.
 	"""
 	ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
-						for i in range(len(rows[0])]
+						for i in range(len(rows[0]))]
 	clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
-							 for i in range(len(rows[0])) for j in range(k)] for j in range(k)]
-	latmatches = None
+							 for i in range(len(rows[0]))] for j in range(k)]
+	lastmatches = None
 
 	for t in range(100):
 		print 'Iteration %d' % t 
-		bestmatche = [[] for in range(k)]
+		bestmatches = [[] for i in range(k)]
 
 		for j in range(len(rows)):
 			row = rows[j]
@@ -86,10 +87,10 @@ It is a kind of K-means clustering function.
 				d = distance(clusters[i], row)
 				if d < distance(clusters[bestmatch], row):
 					bestmatch = i
-				bestmatches[bestmatch].append(j)
-			if bestmatches == latmatches:
-				break
-			latmatches = bestmatches
+			bestmatches[bestmatch].append(j)
+		if bestmatches == lastmatches:
+			break
+		lastmatches = bestmatches
 
 		for i in range(k):
 			avgs = [0.0] * len(rows[0])
@@ -97,7 +98,15 @@ It is a kind of K-means clustering function.
 				for rowid in bestmatches[i]:
 					for m in range(len(rows[rowid])):
 						avgs[m] += rows[rowid][m]
-					for j inh range(len(avgs)):
-						avgs[j] /= len(bestmatches[i])
-					clusters[i] = avgs
-		return bestmatches
+				for j in range(len(avgs)):
+					avgs[j] /= len(bestmatches[i])
+				clusters[i] = avgs
+	return bestmatches
+
+rows = [[random.randint(0, 31) for i in range(32)]
+				 for j in range(100)]
+res = kcluster(rows)
+for cluster in res:
+	for listnum in cluster:
+		print listnum, 
+	print
